194897e44ada6bc2275c9a7108eba78543722ffe
Nanda Kishore <nandakishoreramavath84@gmail.com>
2022-02-10 21:36:59 +0530

    Initial Commit

---
+++ project-3/portfolio_2/assets/iconnect.js
@@ -0,0 +1,35 @@
+function qnaOne() {
+    var x = document.getElementById("hiddenOne");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function qnaTwo() {
+    var x = document.getElementById("hiddenTwo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function qnaThree() {
+    var x = document.getElementById("hiddenThree");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function qnaFour() {
+    var x = document.getElementById("hiddenFour");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
